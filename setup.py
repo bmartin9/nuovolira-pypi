63378aac8b96642e3f63082ae9df4ca1aa5a4e9c
Brendan Martin <b.t.martin@protonmail.com>
2023-06-15 15:59:04 +0100

    rename src to nuovoLIRA

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='nuovoLIRA',
     packages=find_packages(),
     version='0.1.0',
     description='A Bayesian procedure to delineate the boundary of an extended astronomical object',
